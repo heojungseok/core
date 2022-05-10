7de5ae51ac0a374d7b307984f76f4082e18fa1d3
jungseok_heo <tjrwjdgj@gmail.com>
2022-05-10 19:56:05 +0900

    주문과 할인 도메인 설계/ 개발/ 테스트

--- src/main/java/hello/core/member/MemberRepository.java
+++ src/main/java/hello/core/member/MemberRepository.java
@@ -4,5 +4,5 @@ public interface MemberRepository {
 
     void save(Member member); //회원 저장
 
-    Member findBy(Long memberid); //회원의 Id로 회원 찾는 기능
+    Member findById(Long memberid); //회원의 Id로 회원 찾는 기능
 }

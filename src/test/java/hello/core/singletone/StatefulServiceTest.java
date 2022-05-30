8d057c7cd7b67a475ec74c84387b04f6a900065f
jungseok_heo <tjrwjdgj@gmail.com>
2022-05-30 19:54:14 +0900

    싱글톤 방식의 주의점

---
+++ src/test/java/hello/core/singletone/StatefulServiceTest.java
@@ -0,0 +1,34 @@
+package hello.core.singletone;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+        //ThreadA: A 사용자가 10000원 주문
+        statefulService1.order("userA", 10000);
+        //ThreadB: B 사용자가 20000원 주문
+        statefulService2.order("userB", 20000);
+        //10000이여야 하는데 20000으로 되버림 위에 코드로 인해서 order가 가로채짐
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig{
+
+        @Bean
+        public StatefulService statefulService(){
+            return new StatefulService();
+        }
+    }
+
+}

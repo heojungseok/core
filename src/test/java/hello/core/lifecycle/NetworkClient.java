744c37c968f5cbaac50e3923fb0170df2a3e552a
jungseok_heo <tjrwjdgj@gmail.com>
2022-07-11 16:56:09 +0900

    빈 생명주기 - @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -27,14 +30,14 @@ public class NetworkClient {
     public void disconnect(){
         System.out.println("close = " + url);
     }
-
+    @PostConstruct
     public void init() {
         System.out.println("init");
         connect();
         call("초기화 연결 메세지");
 
     }
-
+    @PreDestroy
     public void close() {
         System.out.println("close");
         disconnect();
